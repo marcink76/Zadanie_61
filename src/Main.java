96601c4b0c95269f45b73c14553f0652a38230ab
Marcin Konieczny <marcin_k@coni.pl>
2017-10-02 10:23:06 +0200

    Version 1.0a

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         int[] array = new int[10];
         int[] mirrorArray = new int[10];
@@ -9,23 +9,23 @@ public class Main {
         int count2 = mirrorArray.length - 1;
         int count3 = 0;
 
-        for (count = 0; count < array.length + mirrorArray.length ; count++){
-            if (count <= 9 ) {
+        for (count = 0; count < array.length + mirrorArray.length; count++) {
+            if (count <= 9) {
                 array[count] = Randomize.randomize();
-                System.out.println(count + " " +array[count]);
+                System.out.println(count + " " + array[count]);
             }
             if (count > 9) {
                 mirrorArray[count2] = array[count3];
-                System.out.println(count3 + " "+ mirrorArray[count3]);
+                System.out.println(count3 + " " + mirrorArray[count3]);
                 count3++;
                 count2--;
             }
         }
-        for (int temp:array) {
+        for (int temp : array) {
             System.out.print(temp);
 
         }
-        for (int temp:mirrorArray) {
+        for (int temp : mirrorArray) {
             System.out.print(temp);
         }
 
--- src/Randomize.java
+++ src/Randomize.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 
 public class Randomize {
-    public static int randomize(){
+    public static int randomize() {
         Random random = new Random();
         int rand = random.nextInt(10);
         return rand;
