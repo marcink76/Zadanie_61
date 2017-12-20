8173cf2092f84588bd1a75d437e7f244bfb003fe
Marcin Konieczny <marcin_k@coni.pl>
2017-12-20 12:46:15 +0100

    Version 1.5

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,24 @@
-import java.util.Random;
-
 public class Main {
+
     public static void main(String[] args) {
 
         int[] array = new int[10];
         int[] mirrorArray = new int[10];
-        int count = 0;
+        int count;
         int count2 = mirrorArray.length - 1;
         int count3 = 0;
 
         for (count = 0; count < array.length + mirrorArray.length; count++) {
-            if (count <= 9) {
+            if (count <= array.length - 1) {
                 array[count] = Randomize.randomize();
-                System.out.println(count + " " + array[count]);
+                System.out.println(array[count]);
             }
-            if (count > 9) {
+            if (count > array.length - 1) {
                 mirrorArray[count2] = array[count3];
-                System.out.println(count3 + " " + mirrorArray[count3]);
+                System.out.println(mirrorArray[count3]);
                 count3++;
                 count2--;
             }
         }
-        for (int temp : array) {
-            System.out.print(temp);
-
-        }
-        for (int temp : mirrorArray) {
-            System.out.print(temp);
-        }
-
-
     }
 }
